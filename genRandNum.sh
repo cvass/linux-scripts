3457398c1c79307b431710d0bf1d3f6086af8c3e
cvass <cvassell@gmail.com>
2022-02-23 00:06:41 -0500

    Add files via upload

---
+++ genRandNum.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Script for Adjust.com to output random numbers
+# Written by: Carlton Vassell
+# Date: 2022-02-22
+# Task 1
+
+RANDOM=$$
+
+for i in `seq 10`
+do
+	echo $RANDOM
+done
+
+
